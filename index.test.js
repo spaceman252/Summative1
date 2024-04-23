28a9bf515199d5aad55d33b1af877117ce9a551e
DIPAD-JOE-PHILLIPS <96253001+DIPAD-JOE-PHILLIPS@users.noreply.github.com>
2024-04-23 14:37:43 +0100

    added test for conversion function

---
+++ index.test.js
@@ -0,0 +1,10 @@
+// Assuming the conversion function is exported from another file
+const { conversion } =  require('./app.js')
+
+test('Two integers get multiplied and return a string 2 d.p', () => {
+    expect(conversion(3, 8)).toBe("24.00");
+});
+
+test('Two floats get multiplied and return a string 2 d.p', () => {
+    expect(conversion(4.55, 9.1)).toBe("41.41");
+});
